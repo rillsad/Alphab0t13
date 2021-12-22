8706c30d170f8c2ae6536fca15cfb984f63e652d
rillsad <96515877+rillsad@users.noreply.github.com>
2021-12-22 20:51:08 +0700

    Update zeeoneofc.js

--- zeeone/zeeoneofc.js
+++ zeeone/zeeoneofc.js
@@ -61,7 +61,7 @@ ${numberik}➛Speed : ${latensii.toFixed(4)} Second
 ├❒ Group_support
 └❒ Ownerbot
 
-❒ _Creator BotWea © 2K21_ ❒
+❒ _Creator syhrill © Syhril_ ❒
 `
 }
 const convert = (prefix) => {
@@ -114,7 +114,7 @@ const download = (prefix) => {
 `
 }
 const allmenu = (kyun, os, prefix, wita, wit, ucapannya2, timuu, status, wa_version, mcc, mnc, os_version, device_manufacturer, device_model, alfa , alfa1, thisDay, ini_tanggal, totalchat, hit_today, ini_gcchat, latensii)  => {
-	return`❏───「 *WHATSAPP BOT* 」───❏
+	return`❏───「 *Riltz BOT* 」───❏
                                
 ℒ⃝🌅  *THIS DAY*
 ${ucapannya2}
@@ -969,7 +969,7 @@ ${numberik} ${prefix}yuzuki
 ${numberik} ${prefix}akame
 ${numberik} ${prefix}mikosiba
 
-❏───「 *WHATSAPP BOT* 」───❏
+❏───「 *Riltz BOT* 」───❏
 `
 }
 
